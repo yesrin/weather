1e9a9b8ae3f12bfa43b45625dfec8da819e38998
yesrin <asjs11@naver.com>
2022-01-04 20:33:20 +0900

    상태 hook 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, ChangeEvent } from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [city, setCity] = useState("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCity(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" placeholder="Enter city" onChange={handleChange} />
+        <button type="submit">Get weather</button>
+        <h2>City:{city}</h2>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
