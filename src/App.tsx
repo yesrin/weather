474a38f2e1f41d5122b6d947adacbd7530b175eb
yesrin <asjs11@naver.com>
2022-01-04 22:21:03 +0900

    날씨 api 받아옴

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useEffect } from "react";
 import "./App.css";
+import { Weather } from "./weather";
 
 const App: React.FC = () => {
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState("Korea");
+  const [weather, setWeather] = useState<Weather | null>(null);
+  useEffect(() => {
+    getWeather(city);
+  }, []);
+  const baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+  const suffix = "&units=imperial&appid=30b92d8d96d8f6f8a9a728b8b0d10549";
+
+  const getWeather = async (city: string) => {
+    const response = await fetch(baseUrl + city + suffix);
+    if (response.status === 200) {
+      const jsonWeater = await response.json();
+      const cityTemp: Weather = jsonWeater.main;
+      cityTemp.city = jsonWeater.name;
+      setWeather(cityTemp);
+    } else {
+      setWeather(null);
+    }
+  };
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setCity(event.target.value);
